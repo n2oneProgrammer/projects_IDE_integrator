d2644e1046c3e3d2574b637cb8608eccb890627d
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-03-15 21:28:34 +0100

    project endpoints

--- app/cruds/project_type.py
+++ app/cruds/project_type.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from .. import models
 
 
-def get_editor_by_id(db: Session, editor_id: int):
-    editor = db.query(models.editors.Editors).filter(models.editors.Editors.id == editor_id).first()
+def get_project_type_by_id(db: Session, project_id: int):
+    editor = db.query(models.project_type.ProjectType).filter(models.project_type.ProjectType.id == project_id).first()
 
     return editor
