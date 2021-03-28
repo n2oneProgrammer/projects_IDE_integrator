fc726ae9275caa0861f10870689880c898fe7456
n2oneProgrammer <dominikwojtasik191@gmail.com>
2021-03-28 14:58:05 +0200

    deleting project type endpoint

--- app/routers/project_type.py
+++ app/routers/project_type.py
@@ -1,7 +1,8 @@
 from typing import List
 from sqlalchemy.orm import Session
 from app.dependencies import get_db
-from ..cruds import project_type as project_type_crud, ProjectTypeEdit, get_project_type_by_id, edit_project_type
+from ..cruds import project_type as project_type_crud, ProjectTypeEdit, get_project_type_by_id, edit_project_type, \
+    delete_project_type_by_id
 from ..file_service import ImageException, max_image_size_KB
 from ..schemats.project_type import ProjectTypeResponse, ProjectTypeCreate
 from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
@@ -62,3 +63,16 @@ async def edit_projects_type(
                    "can be up to " + str(max_image_size_KB) + " KB",
         )
     return project_type_response
+
+
+@router.delete("/project_type/{project_type_id}")
+async def delete_projects(
+        project_type_id: int,
+        db: Session = Depends(get_db)
+):
+    project_type = get_project_type_by_id(db, project_type_id)
+    if project_type is None:
+        raise HTTPException(status_code=404, detail="project type not found")
+    delete_project_type_by_id(db, project_type)
+
+    return HTTPException(status_code=200, detail="Project type deleted")
